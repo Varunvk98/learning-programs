dc33dbf20550447e67e0d89532404527af3af291
Varun Kumar N <varunuvce@gmailcom>
2026-03-28 16:33:05 +0530

    StudentPassChecker

--- java/StudentPassFailChecker.java
+++ java/StudentPassFailChecker.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 public class StudentPassFailChecker{
 public static void main(String[] args){
     int[] marks=new int[3];
-    int i=0;
+   // int i=0;
+    //String status="Pass";
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter Student's name :");
     String stdName=sc.nextLine();
     System.out.println("Enter Student's marks obtained");
-    for(i=0;i<=2;i++){
+    float avgMarks=0;
+    boolean x=false;
+    for(int i=0;i<=2;i++){
         marks[i]=sc.nextInt();
+        avgMarks=avgMarks+marks[i];
        // System.out.println("Marks entered:"+marks[i]);
         if(marks[i]<0 || marks[i]>100){
             System.out.println("Invalid mark entered");
             return;
+        }
+        else if (marks[i]<30) {
+            x=true;
         }
     }
     /*Rules
@@ -22,21 +31,21 @@ public static void main(String[] args){
     if mark is not in between 0 and 100, throw ->Invalid mark entered.
 
     */
-   double avgMarks=0;
-   for(i=0;i<=2;i++){
-    if(marks[i]<30){
-        System.out.println("Fail");
-        return;
-    }
-    else{
-        avgMarks=avgMarks+marks[i];
-    }
+   avgMarks=avgMarks/3;
+   BigDecimal bd=new BigDecimal(Double.toString(avgMarks));
+   bd=bd.setScale(1,RoundingMode.HALF_UP);
+   if(avgMarks<50){
+    System.out.println(stdName+" - Average: "+bd+" - FAIL");
    }
-   if((avgMarks/3)>=50){
-    System.out.println("Pass");
+   else if(x){
+    System.out.println(stdName+" - Average: "+bd+" - FAIL (scored below 30 in one subject)");
    }
-   else if((avgMarks/3)<50){
-    System.out.println("Fail");
+   else{
+    System.out.println(stdName+" - Average: "+bd+" - PASS");
    }
+    
+   
+   
+   
 }
 }
